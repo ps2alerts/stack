b2bb4e15faafcee66fa21ab41e7e1875260df88c
Matt Cavanagh <maelstrome26@gmail.com>
2020-08-19 23:50:41 +0100

    Wat

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,11 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     load_config_file = false
+    host             = data.digitalocean_kubernetes_cluster.cluster.endpoint
+    token            = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
+    )
   }
 }
 
