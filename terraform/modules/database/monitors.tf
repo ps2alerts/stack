fa6b5a1c79a48dd2ada3c919bc8c2fb789c1f7aa
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-24 16:27:45 +0100

    Standardized mem monitors

--- terraform/modules/database/monitors.tf
+++ terraform/modules/database/monitors.tf
@@ -1,12 +1,11 @@
-resource datadog_monitor "mongodb_high_mem" {
-  name = "PS2Alerts DB high memory"
+resource datadog_monitor "mongodb_high_cpu" {
+  name = "PS2Alerts DB high CPU"
   type = "metric alert"
-  query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-db} > 996147000"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Mongo", description: "high memory"})
+  query = "avg(last_15m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-db} > 225000000"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Mongo", description: "high CPU"})
 
   thresholds = {
-    critical = 996147000 # 950MB
-    warning = 943718000 # 900MB
+    critical = 225000000 # 0.225 cores
   }
 
   notify_no_data = true
@@ -16,14 +15,14 @@ resource datadog_monitor "mongodb_high_mem" {
   tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "PS2Alerts Mongo"}))
 }
 
-resource datadog_monitor "mongodb_high_cpu" {
-  name = "PS2Alerts DB high CPU"
+resource datadog_monitor "mongodb_high_mem" {
+  name = "PS2Alerts DB high memory"
   type = "metric alert"
-  query = "avg(last_15m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-db} > 225000000"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Mongo", description: "high CPU"})
+  query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-db} > 996147000"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Mongo", description: "high memory"})
 
   thresholds = {
-    critical = 225000000 # 0.225 cores
+    critical = 996147000 # 950MB
   }
 
   notify_no_data = true
