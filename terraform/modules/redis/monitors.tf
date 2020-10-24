8af7d03cb0e90e13c921718efa0518b5ccafc6b6
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-24 15:49:21 +0100

    Added monitors for redis, adjusted CPU and MEM requirements

---
+++ terraform/modules/redis/monitors.tf
@@ -0,0 +1,33 @@
+resource datadog_monitor "redis_high_cpu" {
+  name = "PS2Alerts Redis high CPU"
+  type = "metric alert"
+  query = "avg(last_5m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-redis-master-0} > 40000000"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Redis", description: "high CPU"})
+
+  thresholds = {
+    critical = 40000000 # 0.04 cores
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "PS2Alerts Redis"}))
+}
+
+resource datadog_monitor "redis_high_mem" {
+  name = "PS2Alerts Redis high memory"
+  type = "metric alert"
+  query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-redis-master-0} > 157286000"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Redis", description: "high memory"})
+
+  thresholds = {
+    critical = 157286000 # 150MB
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "PS2Alerts Redis"}))
+}
