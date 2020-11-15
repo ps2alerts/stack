8b2ad3e46ecd6b92c692815ed9e9861d40acb7fc
Matt Cavanagh <maelstrome26@gmail.com>
2020-11-15 15:32:19 +0000

    Added monitor for rabbit restarts, blanked password for rabbitmq

--- terraform/modules/rabbitmq/monitors.tf
+++ terraform/modules/rabbitmq/monitors.tf
@@ -2,7 +2,7 @@ resource datadog_monitor "rabbit_high_cpu" {
   name = "PS2Alerts Rabbit high CPU"
   type = "metric alert"
   query = "avg(last_15m):avg:kubernetes.cpu.usage.total{pod_name:ps2alerts-rabbitmq-0} > 500000000"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Redis", description: "high CPU"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Rabbit", description: "high CPU"})
 
   thresholds = {
     critical = 500000000 # 0.5 cores
@@ -19,7 +19,7 @@ resource datadog_monitor "rabbit_high_mem" {
   name = "PS2Alerts Rabbit high memory"
   type = "metric alert"
   query = "avg(last_5m):avg:kubernetes.memory.rss{pod_name:ps2alerts-rabbitmq-0} > 996147000"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Redis", description: "high memory"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Rabbit", description: "high memory"})
 
   thresholds = {
     critical = 996147000 # 950MB
@@ -36,10 +36,27 @@ resource datadog_monitor "rabbit_online" {
   name = "PS2Alerts Rabbit online"
   type = "metric alert"
   query = "max(last_1m):avg:kubernetes.containers.running{kube_service:ps2alerts-rabbitmq} < 1"
-  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Redis", description: "service offline"})
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Rabbit", description: "service offline"})
 
   thresholds = {
     critical = 1
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "PS2Alerts Rabbit"}))
+}
+
+resource datadog_monitor "rabbit_restarts" {
+  name = "PS2Alerts Rabbit restarts [${var.environment}]"
+  type = "query alert"
+  query = "change(sum(last_5m),last_5m):avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-rabbitmq} > 0.5"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "PS2Alerts Rabbit", description: "restarts"})
+
+  thresholds = {
+    critical = 0.5
   }
 
   notify_no_data = true
