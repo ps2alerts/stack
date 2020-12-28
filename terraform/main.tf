5f68dc86048ec410b4ecfe163414b6a9f6a3d7a7
Matt Cavanagh <maelstrome26@gmail.com>
2020-12-28 01:40:21 +0000

    Fixed formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,8 +21,8 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host             = data.digitalocean_kubernetes_cluster.cluster.endpoint
-    token            = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
+    host  = data.digitalocean_kubernetes_cluster.cluster.endpoint
+    token = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
     cluster_ca_certificate = base64decode(
       data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
     )
