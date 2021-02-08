991c859a7da9d2e7790f538529c1f7bba33991a8
Matt Cavanagh <maelstrome26@gmail.com>
2021-02-08 02:41:42 +0000

    Fixed redis CPU monitor

--- terraform/modules/redis/monitors.tf
+++ terraform/modules/redis/monitors.tf
@@ -1,11 +1,11 @@
 resource datadog_monitor "redis_high_cpu" {
   name = "PS2Alerts Redis high CPU"
   type = "metric alert"
-  query = "avg(last_15m):avg:kubernetes.cpu.usage.total{pod_name:ps2alerts-redis-master-0} > 750000000"
+  query = "avg(last_15m):avg:kubernetes.cpu.usage.total{pod_name:ps2alerts-redis-master-0} > 75000000"
   message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {application: "PS2Alerts Redis", description: "high CPU"})
 
   thresholds = {
-    critical = 750000000 # 0.075 cores
+    critical = 75000000 # 0.075 cores
   }
 
   notify_no_data = true
