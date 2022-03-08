43689decd4d3de2b072d66582d4b8575a5074552
Matt Cavanagh <maelstrome26@gmail.com>
2022-03-08 20:53:07 +0000

    Fixed slightly wonky migration script

--- mongo/migrations/20220301012334-convert_to_adjusted_battle_rank.js
+++ mongo/migrations/20220301012334-convert_to_adjusted_battle_rank.js
@@ -7,7 +7,7 @@ module.exports = {
     console.log("Updating ASP = true characters...");
     const result = await db.collection("aggregate_global_characters").updateMany(filter, updateDoc, options);
     console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)\n`);
-    
+
     console.log("Updating ASP = false characters (Some of these are ASP2 but they should eventually be picked up by death events)...");
     filter["character.asp"] = false;
     updateDoc[0]["$set"]["character.adjustedBattleRank"]["$add"][1] = 0;
@@ -22,8 +22,8 @@ module.exports = {
 
     filter["character.asp"] = undefined;
     console.log("Updating ASP = undefined characters (PS4)...");
-    const result3 = await db.collection("aggregate_global_characters").updateMany(filter, updateDoc, options);
-    console.log(`${result3.matchedCount} document(s) matched the filter, updated ${result3.modifiedCount} document(s)`);
+    const result4 = await db.collection("aggregate_global_characters").updateMany(filter, updateDoc, options);
+    console.log(`${result3.matchedCount} document(s) matched the filter, updated ${result4.modifiedCount} document(s)`);
   },
 
   async down(db) {
@@ -34,7 +34,7 @@ module.exports = {
     console.log("Reverting ASP1 characters...");
     const result = await db.collection("aggregate_global_characters").updateMany(filter, updateDoc, options);
     console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)\n`);
-    
+
     console.log("Reverting non-ASP and ASP2 characters...");
     filter = {$or: [{"character.asp": 0}, {"character.asp": 2}]};
     updateDoc[0]["$set"]["character.asp"] = false;
