2a91cceaf51598b49b2c38b3346405d08db4d3e3
Ryan Sims <ryanjsims@arizona.edu>
2023-03-10 16:23:36 -0700

    Add lattice version migration

---
+++ mongo/migrations/20230310220337-add_lattice_versions.js
@@ -0,0 +1,29 @@
+module.exports = {
+  async up(db) {
+    let filter = {"timeStarted": {"$lte": new Date(2022, 10, 17, 14)}, "latticeVersion": {"$exists": false}};
+    let updateDoc = [{"$set":{"latticeVersion": "$mapVersion"}}];
+
+    console.log(`Updating instances created prior to the November 17, 2022 Update...`);
+    let result = await db.collection("instance_metagame_territories").updateMany(filter, updateDoc);
+    console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)\n`);
+
+    filter = {"timeStarted": {"$gt": new Date(2022, 10, 17, 14)}, "latticeVersion": {"$exists": false}, "zone": {"$ne": 2}};
+    console.log(`Updating unversioned, non-Indar instances created after the November 17, 2022 Update...`);
+    result = await db.collection("instance_metagame_territories").updateMany(filter, updateDoc);
+    console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)\n`);
+
+    filter = {"timeStarted": {"$gt": new Date(2022, 10, 17, 14)}, "latticeVersion": {"$exists": false}, "zone": 2};
+    console.log(`Updating unversioned Indar instances created after the November 17, 2022 Update...`);
+    updateDoc = [{"$set":{"latticeVersion": "1.1"}}];
+    result = await db.collection("instance_metagame_territories").updateMany(filter, updateDoc);
+    console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)\n`);
+  },
+
+  async down(db) {
+    const updateDoc = [{"$unset":{"latticeVersion": 1}}];
+
+    console.log(`Removing latticeVersions from all alert instances...\n`);
+    const result = await db.collection("instance_metagame_territories").updateMany({}, updateDoc);
+    console.log(`Updated ${result.modifiedCount} document(s)\n`);
+  }
+};
